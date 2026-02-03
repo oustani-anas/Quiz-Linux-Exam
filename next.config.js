11edc04d0ae947857a1bdef3140440e239695985
oustani-anas <anas.migos20@gmail.com>
2026-02-03 20:16:14 +0100

    add analytics ...

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  eslint: {
+    // Disable ESLint during builds (use separate tooling if needed)
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
